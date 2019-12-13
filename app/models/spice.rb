32362e4a40e53c528d1434559f20a31961d862a2
jaemcu <cupp.j.e@gmail.com>
2019-12-13 09:56:54 -0500

    Tenth Commit: Added Macros to Models and Serializers
    Added belongs_to and has_many to Spices and User, respectively
    "user" model AND serializer: has_many :spices
    "spice" model AND serializer: belongs_to :user
    "spice" serializer: add editable field

--- app/models/spice.rb
+++ app/models/spice.rb
@@ -1,2 +1,3 @@
 class Spice < ApplicationRecord
+  belongs_to :user
 end
--- app/serializers/spice_serializer.rb
+++ app/serializers/spice_serializer.rb
@@ -1,3 +1,7 @@
 class SpiceSerializer < ActiveModel::Serializer
   attributes :id, :name, :origin, :culinary_use, :weight, :price
+  belongs_to :user
+  def editable
+    scope == object.user
+  end
 end
